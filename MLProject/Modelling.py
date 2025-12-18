edb1bf553e63c09b66acb1f32d6ce7788d08e999
iwayanrestu <143016139+iwayanrestu@users.noreply.github.com>
2025-12-18 16:11:14 +0700

    Refactor KMeans clustering with MLflow autologging
    Refactor KMeans clustering code to use autologging with MLflow.

--- MLProject/Modelling.py
+++ MLProject/Modelling.py
@@ -1,23 +1,22 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
+
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
-mlflow.set_experiment("Credit Card Clustering")
-
+# Load data
 df = pd.read_csv("credit_card_scaled.csv")
 X = df.values
 
-with mlflow.start_run():
-    kmeans = KMeans(n_clusters=4, random_state=42)
-    kmeans.fit(X)
+mlflow.set_experiment("Credit Card Clustering")
+mlflow.sklearn.autolog()
 
-    labels = kmeans.labels_
-    silhouette = silhouette_score(X, labels)
+kmeans = KMeans(n_clusters=4, random_state=42)
+kmeans.fit(X)
 
-    mlflow.log_param("n_clusters", 4)
-    mlflow.log_metric("silhouette_score", silhouette)
-    mlflow.sklearn.log_model(kmeans, "model")
+labels = kmeans.labels_
+silhouette = silhouette_score(X, labels)
 
-print("Training finished")
+mlflow.log_metric("silhouette_score", silhouette)
+print("Silhouette Score:", silhouette)

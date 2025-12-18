4a547b2d396a16c23b52e2c32d32d2321a30664c
iwayanrestu <143016139+iwayanrestu@users.noreply.github.com>
2025-12-18 14:05:34 +0700

    Update Modelling.py

--- MLProject/Modelling.py
+++ MLProject/Modelling.py
@@ -1,22 +1,25 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
-# Load data
+# WAJIB SET TRACKING URI
+mlflow.set_tracking_uri("file:./mlruns")
+mlflow.set_experiment("Credit Card Clustering")
+
 df = pd.read_csv("credit_card_scaled.csv")
 X = df.values
 
-mlflow.set_experiment("Credit Card Clustering")
-mlflow.sklearn.autolog()
+with mlflow.start_run(nested=True):
+    kmeans = KMeans(n_clusters=4, random_state=42)
+    kmeans.fit(X)
 
-kmeans = KMeans(n_clusters=4, random_state=42)
-kmeans.fit(X)
+    labels = kmeans.labels_
+    silhouette = silhouette_score(X, labels)
 
-labels = kmeans.labels_
-silhouette = silhouette_score(X, labels)
+    mlflow.log_param("n_clusters", 4)
+    mlflow.log_metric("silhouette_score", silhouette)
+    mlflow.sklearn.log_model(kmeans, artifact_path="model")
 
-mlflow.log_metric("silhouette_score", silhouette)
-print("Silhouette Score:", silhouette)
+print("Training finished")
